0279cda0bd7c542bec78758c0dcba282daf3a126
Mutsuha Asada <me@momee.mt>
2024-10-08 17:14:41 +0900

    std.pandoc: init

---
+++ flake.nix
@@ -0,0 +1,85 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+        target = "wasm32-unknown-unknown";
+        toolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [target];
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = toolchain;
+          cargo = toolchain;
+        };
+      in {
+        packages.default = let
+          name = "std-html";
+          wasmPackage = rustPlatform.buildRustPackage {
+            inherit name;
+            src = ./.;
+            copyLibs = true;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            CARGO_BUILD_TARGET = target;
+            CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "${pkgs.llvmPackages.lld}/bin/lld";
+            postBuild = ''
+              for file in $(find $out -name "*.wasm"); do
+                base=$(basename $file .wasm)
+                renamed=$(echo $base | sed 's/_/./g').wasm
+                mv $file $(dirname $file)/$renamed
+              done
+            '';
+            buildPhase = ''
+              cargo build --release -p ${name} --target=${target}
+            '';
+            installPhase = ''
+              mkdir -p $out/lib
+              cp target/${target}/release/*.wasm $out/lib/
+            '';
+          };
+        in
+          pkgs.stdenv.mkDerivation {
+            name = "brack-std-pandoc";
+            buildInputs = [pkgs.rename];
+            inherit (wasmPackage) src;
+            installPhase = ''
+              mkdir -p $out
+              cp -r ${wasmPackage}/lib/* $out/
+              for file in $out/*_*.wasm; do
+                mv "$file" "$(echo $file | sed 's/_/./g')"
+              done
+            '';
+          };
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            toolchain
+            rust-analyzer
+            nil
+            alejandra
+            extism-cli
+            pandoc
+          ];
+        };
+      }
+    );
+}
